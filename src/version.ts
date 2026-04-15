1448e36310d9fc4dd7854672f9a20712816a0fa9
cgil <lao.tseu.is.alive@gmail.com>
2026-04-15 15:05:54 +0200

    added point edition of drawing entities

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
-export const LIB_VERSION = "0.0.3";
+export const LIB_VERSION = "0.0.4";
 
-export const BUILD_TIME = "2026-04-15T11:42:06.967Z";
+export const BUILD_TIME = "2026-04-15T13:05:48.286Z";
